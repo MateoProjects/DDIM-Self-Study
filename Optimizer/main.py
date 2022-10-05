73f564e4b7b9295f0b420cecd995dfd3a2ab3b6b
Ramon Mateo Navarro <mateo.trb@gmail.com>
2022-10-05 11:58:31 +0200

    Add Scripts for Optimizers

---
+++ Optimizer/main.py
@@ -0,0 +1,4 @@
+from stable_diffusion import StableDiffusion
+
+
+stable = StableDiffusion(precision_high=False)
---
+++ Optimizer/stable_diffusion.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from torch import autocast
+from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
+import torch
+
+class StableDiffusion():
+    def __init__(self, precision_high):
+        with open('Optimizer\\token.txt') as f:
+            token = f.readline()
+        if precision_high:
+            self.pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=token)
+        else:
+            self.pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16, use_auth_token=token)
+
+
+    def generate_image(self,prompt, num_inference_steps=50, guidance_scale=7.5, seedGen=1023):
+        generator = torch.Generator("cuda").manual_seed(seedGen)
+        self.image = self.pipe(prompt, height=256, width=256, 
+                num_inference_steps=num_inference_steps,
+                guidance_scale=guidance_scale,
+                generator=generator)["sample"]
+        return self.image
